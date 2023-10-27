8d79d5a695e6ae5d2d8951a4c23065af0ceaecc4
SarahRosniani <sarahrosniani679@gmail.com>
2023-10-27 10:01:17 +0700

    update-delete

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -108,4 +108,57 @@ class UserController extends BaseController
 
         return view('profile', $data);
     }
+
+    public function edit($id){
+        $user = $this->userModel->getUsers($id);
+        $kelas = $this->kelasModel->getKelas();
+
+        $data = [
+            'title' => 'Edit Data',
+            'user'  => $user,
+            'kelas' => $kelas,
+        ];
+
+        return view('edit_user', $data);
+    }
+
+    public function update($id){
+        $path = 'assets/uploads/img/' ;
+
+        $foto = $this->request->getFile('foto');
+
+        if ($foto->isValid()){
+            $name = $foto->getRandomName();
+
+        if($foto->move($path, $name)){
+            $foto = base_url($path . $name);
+        }
+    }
+
+    $data = [
+        'nama'  => $this->request->getVar('nama'),
+        'id_kelas'  => $this->request->getVar('kelas'),
+        'npm'  => $this->request->getVar('npm'),
+        'foto'  => $foto_path
+        
+    ];
+
+    $result = $this->userModel->updateUser($data, $id);
+
+    if(!$result){
+        return redirect()->back()->withInput()
+            ->with('error', 'Gagal Menympan Data');
+    }
+
+    return redirect()->to('/user');
+    }
+
+    public function destroy($id){
+        $result = $this->userModel->deleteUser($id);
+        if(!$result){
+            return redirect()->back()->with('error', 'Gagal Menghapus data');
+        }
+        return redirect()->to(base_url('/user'))
+            ->with('succes', 'Berhasil Menghapus Data');
+    }
 }
